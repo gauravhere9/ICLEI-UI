00518768dc53ffe69257eb020784e0a04ebff948
Bipin Kumar <Bkumar455@gmail.com>
2023-05-16 12:15:44 +0530

    added user controller

---
+++ WebApp/WebApp.UI/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Global.Options;
+
+namespace WebApp.UI.Controllers
+{
+	[Route("/users")]
+	public class UsersController : BaseController
+	{
+		private readonly ApplicationOptions _applicationOptions;
+		public UsersController(ApplicationOptions applicationOptions) : base(applicationOptions)
+		{
+			_applicationOptions = applicationOptions;
+		}
+
+		[HttpGet]
+		[Route("")]
+		public async Task<IActionResult> Index()
+		{
+			return await Task.Run(() => View());
+		}
+	}
+}

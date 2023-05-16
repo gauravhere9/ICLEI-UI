d91a06fd071e8b5c9c9d80ae6824f02c41751286
Bipin Kumar <Bkumar455@gmail.com>
2023-05-16 15:47:32 +0530

    Fixed a bug in authentication middleware

--- WebApp/WebApp.UI/Core/Middlewares/AuthenticationMiddleware.cs
+++ WebApp/WebApp.UI/Core/Middlewares/AuthenticationMiddleware.cs
@@ -11,232 +11,237 @@ using WebApp.Global.Shared;
 
 namespace WebApp.UI.Core.Middlewares
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public class AuthenticationMiddleware
-    {
-        private readonly RequestDelegate _next;
+	/// <summary>
+	/// 
+	/// </summary>
+	public class AuthenticationMiddleware
+	{
+		private readonly RequestDelegate _next;
 
-        private readonly AuthenticationOptions _authenticationOptions;
-        private static ClaimsIdentity? _identity;
+		private readonly AuthenticationOptions _authenticationOptions;
+		private static ClaimsIdentity? _identity;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="next"></param>
-        /// <param name="authenticationOptions"></param>
-        /// <param name="userService"></param>
-        public AuthenticationMiddleware(RequestDelegate next, AuthenticationOptions authenticationOptions)
-        {
-            _next = next;
-            _authenticationOptions = authenticationOptions;
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="next"></param>
+		/// <param name="authenticationOptions"></param>
+		/// <param name="userService"></param>
+		public AuthenticationMiddleware(RequestDelegate next, AuthenticationOptions authenticationOptions)
+		{
+			_next = next;
+			_authenticationOptions = authenticationOptions;
 
-        }
+		}
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public async Task Invoke(HttpContext context)
-        {
-            if (_authenticationOptions.Enabled)
-            {
-                var key = "X-Access-Token";
-                var tokenDetails = context.Request.Cookies[key];
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="context"></param>
+		/// <returns></returns>
+		public async Task Invoke(HttpContext context)
+		{
+			if (_authenticationOptions.Enabled)
+			{
+				var key = "X-Access-Token";
+				var tokenDetails = context.Request.Cookies[key];
 
-                var endpoint = context.GetEndpoint();
-                if (endpoint == null)
-                {
-                    if (string.IsNullOrWhiteSpace(tokenDetails))
-                    {
-                        context.Response.Redirect("/");
-                        await Task.CompletedTask;
-                        return;
-                    }
-                    else
-                    {
-                        context.Response.Redirect("/dashboard");
-                        await Task.CompletedTask;
-                        return;
-                    }
-                    //await _next(context);
-                }
-
-                if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() is object)
-                {
-                    if (string.IsNullOrWhiteSpace(tokenDetails))
-                    {
-                        await _next(context);
-                        await Task.CompletedTask;
-                        return;
-                    }
-                    else
-                    {
-                        context.Response.Redirect("/dashboard");
-                        await Task.CompletedTask;
-                        return;
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(tokenDetails))
-                {
-                    var loginResponseDto = Newtonsoft.Json.JsonConvert.DeserializeObject<LoginResponseDto>(tokenDetails);
-
-                    if (loginResponseDto != null)
-                    {
-                        if (!string.IsNullOrWhiteSpace(loginResponseDto.Token))
-                        {
-                            var principal = await AuthenticateJwtToken(loginResponseDto.Token);
-
-                            if (principal != null)
-                            {
-                                context.User = principal as ClaimsPrincipal;
-
-                                if (context.User?.Identity?.IsAuthenticated == true)
-                                {
-                                    await _next.Invoke(context);
-                                    await Task.CompletedTask;
-                                    return;
-                                }
-                            }
-                        }
-                    }
-
-                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-                    context.Response.Redirect("/401-unauthorized");
-                    await Task.CompletedTask;
-                    return;
-                }
-                else
-                {
-                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                    context.Response.Redirect("/");
-                    await Task.CompletedTask;
-                    return;
-                }
-            }
-            else
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.OK;
-                await _next.Invoke(context);
-                await Task.CompletedTask;
-                return;
-            }
-        }
+				var endpoint = context.GetEndpoint();
+				if (endpoint == null)
+				{
+					if (string.IsNullOrWhiteSpace(tokenDetails))
+					{
+						//context.Response.Redirect("/");
+						//await Task.CompletedTask;
+						//return;
 
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        protected async Task<IPrincipal?> AuthenticateJwtToken(string token)
-        {
-            var result = await ValidateToken(token);
-            if (result.isValid)
-            {
-                if (result.tokenValidationResult != null)
-                {
-                    if (result.tokenValidationResult.IsValid)
-                    {
-                        if (result.tokenValidationResult.Claims != null)
-                        {
-                            var claims = result.tokenValidationResult.Claims;
+						await _next(context);
+						await Task.CompletedTask;
+						return;
+					}
+					else
+					{
+						context.Response.Redirect("/dashboard");
+						await Task.CompletedTask;
+						return;
+					}
+					//await _next(context);
+				}
 
-                            var firstName = claims.FirstOrDefault(x => x.Key == ClaimTypes.Name).Value.ToString() ?? string.Empty;
-                            var email = claims.FirstOrDefault(x => x.Key == ClaimTypes.Email).Value.ToString() ?? string.Empty;
-                            int.TryParse(claims.FirstOrDefault(x => x.Key == ClaimTypes.Actor).Value.ToString(), out int userId);
+				if (endpoint?.Metadata?.GetMetadata<IAllowAnonymous>() is object)
+				{
+					if (string.IsNullOrWhiteSpace(tokenDetails))
+					{
+						await _next(context);
+						await Task.CompletedTask;
+						return;
+					}
+					else
+					{
+						context.Response.Redirect("/dashboard");
+						await Task.CompletedTask;
+						return;
+					}
+				}
 
-                            CustomPrincipal userDetails = new CustomPrincipal(email)
-                            {
-                                Id = userId,
-                                FullName = firstName,
-                                Email = email,
-                                Token = token
-                            };
+				if (!string.IsNullOrWhiteSpace(tokenDetails))
+				{
+					var loginResponseDto = Newtonsoft.Json.JsonConvert.DeserializeObject<LoginResponseDto>(tokenDetails);
 
-                            IPrincipal? user = userDetails;
-                            return user;
-                        }
+					if (loginResponseDto != null)
+					{
+						if (!string.IsNullOrWhiteSpace(loginResponseDto.Token))
+						{
+							var principal = await AuthenticateJwtToken(loginResponseDto.Token);
 
-                    }
-                }
-            }
-            return null;
-        }
+							if (principal != null)
+							{
+								context.User = principal as ClaimsPrincipal;
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        public async Task<(bool isValid, TokenValidationResult tokenValidationResult)> ValidateToken(string token)
-        {
-            var validationResult = await GetTokenValidationResult(token);
+								if (context.User?.Identity?.IsAuthenticated == true)
+								{
+									await _next.Invoke(context);
+									await Task.CompletedTask;
+									return;
+								}
+							}
+						}
+					}
 
-            _identity = validationResult?.ClaimsIdentity;
+					context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+					context.Response.Redirect("/401-unauthorized");
+					await Task.CompletedTask;
+					return;
+				}
+				else
+				{
+					context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+					context.Response.Redirect("/");
+					await Task.CompletedTask;
+					return;
+				}
+			}
+			else
+			{
+				context.Response.StatusCode = (int)HttpStatusCode.OK;
+				await _next.Invoke(context);
+				await Task.CompletedTask;
+				return;
+			}
+		}
 
-            if (_identity == null)
-                return (false, validationResult);
 
-            if (!_identity.IsAuthenticated)
-                return (false, validationResult);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="token"></param>
+		/// <returns></returns>
+		protected async Task<IPrincipal?> AuthenticateJwtToken(string token)
+		{
+			var result = await ValidateToken(token);
+			if (result.isValid)
+			{
+				if (result.tokenValidationResult != null)
+				{
+					if (result.tokenValidationResult.IsValid)
+					{
+						if (result.tokenValidationResult.Claims != null)
+						{
+							var claims = result.tokenValidationResult.Claims;
 
-            return (true, validationResult);
-        }
+							var firstName = claims.FirstOrDefault(x => x.Key == ClaimTypes.Name).Value.ToString() ?? string.Empty;
+							var email = claims.FirstOrDefault(x => x.Key == ClaimTypes.Email).Value.ToString() ?? string.Empty;
+							int.TryParse(claims.FirstOrDefault(x => x.Key == ClaimTypes.Actor).Value.ToString(), out int userId);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        public async Task<TokenValidationResult?> GetTokenValidationResult(string token)
-        {
-            var tokenHandler = new JwtSecurityTokenHandler();
+							CustomPrincipal userDetails = new CustomPrincipal(email)
+							{
+								Id = userId,
+								FullName = firstName,
+								Email = email,
+								Token = token
+							};
 
-            if (!(tokenHandler.ReadToken(token) is JwtSecurityToken jwtToken)) return null;
+							IPrincipal? user = userDetails;
+							return user;
+						}
 
-            string secret = _authenticationOptions.JWTToken.Secret;
+					}
+				}
+			}
+			return null;
+		}
 
-            var Key = Encoding.ASCII.GetBytes(secret);
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="token"></param>
+		/// <returns></returns>
+		public async Task<(bool isValid, TokenValidationResult tokenValidationResult)> ValidateToken(string token)
+		{
+			var validationResult = await GetTokenValidationResult(token);
 
-            var symmetricKey = new SymmetricSecurityKey(Key);
+			_identity = validationResult?.ClaimsIdentity;
 
-            var validationParameters = new TokenValidationParameters()
-            {
-                RequireExpirationTime = true,
-                ValidateIssuer = true,
-                ValidateAudience = false,
-                IssuerSigningKey = symmetricKey,
-                ValidateIssuerSigningKey = true,
-                ClockSkew = TimeSpan.FromMinutes(0),
-                ValidIssuer = _authenticationOptions.JWTToken.Issuer
+			if (_identity == null)
+				return (false, validationResult);
 
-            };
+			if (!_identity.IsAuthenticated)
+				return (false, validationResult);
 
-            //ClaimsPrincipal principal = tokenHandler.ValidateToken(token, validationParameters, validatedToken: out SecurityToken securityToken);
-            TokenValidationResult? result = await tokenHandler.ValidateTokenAsync(token, validationParameters);
+			return (true, validationResult);
+		}
 
-            return result;
-        }
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="token"></param>
+		/// <returns></returns>
+		public async Task<TokenValidationResult?> GetTokenValidationResult(string token)
+		{
+			var tokenHandler = new JwtSecurityTokenHandler();
 
-    }
+			if (!(tokenHandler.ReadToken(token) is JwtSecurityToken jwtToken)) return null;
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public static class AuthenticationMiddlewareExtensions
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="builder"></param>
-        /// <returns></returns>
-        public static IApplicationBuilder UseAuthenticationMiddleware(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<AuthenticationMiddleware>();
-        }
-    }
+			string secret = _authenticationOptions.JWTToken.Secret;
+
+			var Key = Encoding.ASCII.GetBytes(secret);
+
+			var symmetricKey = new SymmetricSecurityKey(Key);
+
+			var validationParameters = new TokenValidationParameters()
+			{
+				RequireExpirationTime = true,
+				ValidateIssuer = true,
+				ValidateAudience = false,
+				IssuerSigningKey = symmetricKey,
+				ValidateIssuerSigningKey = true,
+				ClockSkew = TimeSpan.FromMinutes(0),
+				ValidIssuer = _authenticationOptions.JWTToken.Issuer
+
+			};
+
+			//ClaimsPrincipal principal = tokenHandler.ValidateToken(token, validationParameters, validatedToken: out SecurityToken securityToken);
+			TokenValidationResult? result = await tokenHandler.ValidateTokenAsync(token, validationParameters);
+
+			return result;
+		}
+
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class AuthenticationMiddlewareExtensions
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <returns></returns>
+		public static IApplicationBuilder UseAuthenticationMiddleware(this IApplicationBuilder builder)
+		{
+			return builder.UseMiddleware<AuthenticationMiddleware>();
+		}
+	}
 }
